f28f593c71fc838cb4ffbd3a64f058d0a197283e
Burt <2902978956@qq.com>
2022-08-13 17:55:50 +0800

    开启 JSX（TSX）功能

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+<script lang="tsx">
 import HelloWorld from './components/HelloWorld.vue'
+import {defineComponent, ref} from 'vue';
+import vueIcon from './assets/vue.svg';
+
+export default defineComponent({
+  setup() {
+    return () => (
+      <>
+        <div>
+          <a href="https://vitejs.dev" target="_blank" onClick={(e) => {
+            e.preventDefault()
+            console.log('!!!!!');
+          }}>
+            <img src="/vite.svg" class="logo" alt="Vite logo" />
+          </a>
+          <a href="https://vuejs.org/" target="_blank">
+            <img src={vueIcon} class="logo vue" alt="Vue logo" />
+          </a>
+        </div>
+        <HelloWorld msg="Vite + Vue" />
+      </>
+    )
+  }
+})
 </script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
 
 <style scoped>
 .logo {
